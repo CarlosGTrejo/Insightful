0e496e4c8fa2d461810d7033dbab0a0f384d11d7
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2023-03-26 22:15:51 -0500

    loading audio, and processing using ffmpeg

--- backend/audio_utils.py
+++ backend/audio_utils.py
@@ -1,4 +1,43 @@
+import ffmpeg
+import numpy as np
 
 
-def hello_world():
-    pass
+def audio_to_flac(audio_bytes: bytes) -> bytes:
+    '''Converts mp3 to flac with CUDA hardware acceleration'''
+    process = (
+        ffmpeg
+        .input('pipe:', threads=0)
+        .output('pipe:', format='flac')
+        .run_async(pipe_stdin=True, pipe_stdout=True)
+    )
+
+    out, _ = process.communicate(input=audio_bytes)  # process mp3_bytes
+    return out
+
+
+def load_audio(audio_bytes: bytes, sr: int = 16_000) -> np.ndarray:
+    """
+    Use file's bytes and transform to mono waveform, resampling as necessary
+    Parameters
+    ----------
+    audio_bytes: bytes
+        The bytes of the audio file
+    sr: int
+        The sample rate to resample the audio if necessary
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input('pipe:', threads=0)
+            .output("pipe:", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run_async(pipe_stdin=True, pipe_stdout=True)
+        ).communicate(input=audio_bytes)
+
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
