2de73c9dee03a362d2b118094e3f3efebac2c3bb
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2023-03-28 13:21:19 -0500

    added loading, logging, used event.data

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,39 @@
 <script>
   let file;
   let socket;
-  let audio_file;
+  let transcript;
   let elapsed_time;
+  let loading = false;
+  let start_time;
 
   async function handleFileUpload(event) {
     file = event.target.files[0];
+    loading = true;
 
     if (!socket || socket.readyState !== WebSocket.OPEN) {
       socket = new WebSocket("ws://localhost:8000/ws");
+
+      socket.addEventListener("open", (event) => {
+        start_time = performance.now();  // Start perf counter
+        socket.send(file)  // Send file to server
+      })
+
+      // Error handling
+      socket.addEventListener("error", (event) => {
+        console.error(event)
+      })
+    } else {
+      socket.send(file);  // Send file if socket is open already
     }
 
-    socket.binaryType = "arraybuffer";
-
-    socket.addEventListener("open", (event) => {
-      socket.send(file);
-    });
-    // Count processing time after file is sent.
-    const start_time = performance.now();
-
+    // Receive data
     socket.addEventListener("message", (event) => {
-      audio_file = new Blob([event.data], { type: "audio/mpeg" });
+      console.dir(event)
+      transcript = event.data;
+
+      loading = false
       const end_time = performance.now();
+
       // Calculate processing time
       elapsed_time = Math.round(end_time - start_time);
       console.log(`%c ⏱️ Processing time: ${elapsed_time.toLocaleString("en-US")}ms`, 'background-color: yellow; color: black; font-weight: bold; font-size: 30px; font-family: "Ubuntu Mono", monospace, sans-serif')
@@ -31,8 +43,10 @@
 </script>
 
 <main>
-  <input type="file" accept="audio/mpeg" on:change={handleFileUpload} />
-  {#if audio_file}
-    <a href={URL.createObjectURL(audio_file)} download="audio.flac">Download Processed Audio</a>
+  <input type="file" accept="audio/*" on:change={handleFileUpload} />
+  {#if loading}
+    <p>Loading...</p>
+  {:else}
+    <p>{transcript ? transcript: 'No transcript available'}</p>
   {/if}
 </main>
