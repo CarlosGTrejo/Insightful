1cf0bfe60d718a99e950a01cb5d47afc3df342f9
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2023-04-24 01:55:34 -0500

    Added Summary Tile and text styles

--- src/App.svelte
+++ src/App.svelte
@@ -6,27 +6,28 @@
     SideNav,
     SideNavItems,
     SideNavLink,
+    Content,
+    Grid,
+    Row,
+    Column,
+    Tile
   } from "carbon-components-svelte";
 
   let isSideNavOpen = false;
-  let chapters
-  let title
-  //let chapters = ["FitnessGram","Pacer","Test"];
-  //let title = "FitnessGram Pacer Test";
+  let chapters = [];
+  let title;
 
   let processed = false;
   let file;
   let socket;
-  let transcript
-  //let transcript = "The FitnessGram pacer test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start. ding ";
-  let elapsed_time;
-  let start_time;
+  let transcript;
+  let elapsed_time; // Used to measure perf
+  let start_time;  // Used to measure perf
 
   async function handleFileUpload(event) {
-    console.dir(event)
-    file = event.detail[0];
+    file = event.detail[0];  // Get file from event
 
-    if (!socket || socket.readyState !== WebSocket.OPEN) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {  // Open up a socket if there isn't one open already, then send file
       socket = new WebSocket("ws://localhost:8000/ws");
 
       socket.addEventListener("open", (event) => {
@@ -34,36 +35,55 @@
         socket.send(file)  // Send file to server
       })
 
-      // Error handling
+      // Error handling (log errors to console)
       socket.addEventListener("error", (event) => {
         console.error(event)
       })
     } else {
-      socket.send(file);  // Send file if socket is open already
+      socket.send(file);  // Send file if a socket is already opened
     }
 
-    // Receive data
+    // Receive data from backend
     socket.addEventListener("message", (event) => {
       console.dir(event)
       transcript = event.data;
 
-      const end_time = performance.now();
+      const end_time = performance.now();  // Stop perf counter
 
-      // Calculate processing time
+      // Calculate processing time and log to console
       elapsed_time = Math.round(end_time - start_time);
       console.log(`%c ⏱️ Processing time: ${elapsed_time.toLocaleString("en-US")}ms`, 'background-color: yellow; color: black; font-weight: bold; font-size: 30px; font-family: "Ubuntu Mono", monospace, sans-serif')
-      processed = true;
+      processed = true;  // File is done being processed, set to true so that transcript shows up.
     });
   }
 </script>
 
 <!-- Issues arose when main was styled as a whole. Each component is stylized inidividually as a result. -->
-<!--<style>
-  main {
-    
+<style lang="scss">
+  @import '@carbon/type';
+
+  .file-uploader-wrapper { // Used to center FileUpload component
+    display: grid;
+    place-items: center;
+    height: 90vh;
   }
+
+  h1 {
+    @include type-style('heading-01');
+    // color: #6F6F6F;
+  }
+  p {
+    @include type-style('body-02');
+    margin-top: 1em;
+  }
+  audio {
+    height: 2.5em;
+    margin-top: 1em;
+    min-width: 100%;
+  }
+
 </style>
--->
+
 
 <Header company="" platformName="Insightful" bind:isSideNavOpen>
   <svelte:fragment slot="skip-to-content">
@@ -71,31 +91,44 @@
   </svelte:fragment>
 </Header>
 
-<main>
-  {#if !processed}
-  <div style="display: grid; place-items: center; height: 100vh; padding: auto;">
-    <FileUploader
-      labelTitle="Upload file"
-      buttonLabel="Add file"
-      labelDescription="Only audio files are accepted."
-      accept={["audio/*"]}
-      on:change={handleFileUpload}
-    />
-  </div>
-  {:else}
-  <!-- TODO: Style transcript to display like in the Figma design and display "chapters" on the left side with times stamps (eg. use the variable "chapters" which will be an array that holds the chapter title and timestamp as a string, use simulated data if necessary for now) -->
-  <!-- Chapters in the side navigation bar -->
-    <SideNav bind:isOpen={isSideNavOpen}>
-      <SideNavItems>
-        {#each chapters as chapter}
-          <SideNavLink text={chapter} />
-        {/each}
-      </SideNavItems>
-    </SideNav>
-  <!-- Transcript, as well as generated title -->
-    <div style="display: grid; place-items: start left; color:#6f6f6f; padding-left:304px; padding-right:32vw; padding-top: 100px;">
-      <p><strong>{title}</strong></p>
-      <p>{transcript ? transcript: 'No transcript available'}</p>
-    </div>
-  {/if}
-</main>
+
+{#if !processed}
+    <Content>
+      <div class="file-uploader-wrapper">
+        <FileUploader
+          labelTitle="Upload file"
+          buttonLabel="Add file"
+          labelDescription="Only audio files are accepted."
+          accept={["audio/*"]}
+          on:change={handleFileUpload}
+        />
+      </div>
+    </Content>
+{:else}
+<!-- Add chapters to the side bar -->
+  <SideNav bind:isOpen={isSideNavOpen}>
+    <SideNavItems>
+      {#each chapters as chapter}
+        <SideNavLink text={chapter} />
+      {/each}
+    </SideNavItems>
+  </SideNav>
+  <Content>
+    <Grid>
+      <Row>
+        <!-- Transcript + audio player -->
+        <Column sm={2} md={8} lg={10}>
+          <h1>{title}</h1>
+          <audio controls src="{URL.createObjectURL(file)}"></audio>
+          <p>{transcript ? transcript: 'No transcript available'}</p>
+        </Column>
+        <Column sm={1} md={4} lg={5}>
+          <Tile>
+            <h1>Summary</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam, nesciunt nostrum qui quis provident veritatis minus deserunt voluptate sequi aperiam? Facere officia quam tenetur labore optio nam esse quae.</p>
+          </Tile>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+{/if}
