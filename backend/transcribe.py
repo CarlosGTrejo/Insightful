57da5965da01ca062f58688232d04bb7f25bc0c5
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2023-04-26 21:04:32 -0500

    began integrating deepgram + minor refactoring

--- backend/transcribe.py
+++ backend/transcribe.py
@@ -1,5 +1,14 @@
-import whisper
+import asyncio
+
 import audio_utils
+import utils
+import whisper
+from deepgram import Deepgram
+
+
+# Get api key
+with open('deepgram_key', 'r') as f:
+    key = f.read().strip()
 
 options = {
     'language': 'en',
@@ -9,10 +18,38 @@ options = {
 model = whisper.load_model('base.en', device='cuda', download_root='./models')
 
 
-def transcription_pipeline(audio_bytes: bytes) -> str:
+def using_whisper(audio_bytes: bytes) -> str:
     audio = audio_utils.load_audio(audio_bytes)
     result = model.transcribe(audio, **options)
     transcript = result.get('text')
     if not isinstance(transcript, str):
         transcript = 'No transcript available'
     return transcript
+
+
+async def using_deepgram(audio: bytes, mimetype: str):
+    deepgram = Deepgram(key)
+
+    source = {
+        'buffer': audio,
+        'mimetype': mimetype
+    }
+    options = {
+        'punctuate': True,
+        'summarize': True,
+        'numerals': True,
+        'model': 'nova',
+        'language': 'en'
+    }
+
+    response = await asyncio.create_task(
+        deepgram.transcription.prerecorded(
+            source,
+            options
+        )
+    )
+
+    summary = utils.get_summary(response)
+    transcript = utils.get_transcript(response)
+
+    return transcript, summary
